c6724a0383952e2acc9dfb2f4c7e56daf9e6ffa7
Mo Gorhom <gorhom@me.com>
2020-06-14 11:39:20 +0200

    chore: updated ios splash screen

--- example/ios/PaperOnboardingExample/AppDelegate.m
+++ example/ios/PaperOnboardingExample/AppDelegate.m
@@ -41,7 +41,7 @@ static void InitializeFlipper(UIApplication *application) {
                                                    moduleName:@"PaperOnboardingExample"
                                             initialProperties:nil];
 
-  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:0.0f green:0.0f blue:0.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
