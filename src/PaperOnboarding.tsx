57ec1b2eb10a779b025c4523ddf3fb46c8b65ae7
Mo Gorhom <gorhom@me.com>
2021-07-28 21:22:48 +0100

    feat: added next & previous methods (#34)
    * feat: added next & previous public methods
    
    * docs: updated readme

--- src/PaperOnboarding.tsx
+++ src/PaperOnboarding.tsx
@@ -1,4 +1,12 @@
-import React, { useMemo, useRef, useCallback, memo, useState } from 'react';
+import React, {
+  useMemo,
+  useRef,
+  useCallback,
+  memo,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+} from 'react';
 import {
   Dimensions,
   Insets,
@@ -30,6 +38,7 @@ import {
 import { styles } from './styles';
 import type {
   PaperOnboardingProps,
+  PaperOnboardingMethods,
   PaperOnboardingScreenDimensions,
 } from './types';
 
@@ -48,183 +57,229 @@ const {
 } = require('react-native-reanimated');
 const interpolate = interpolateV2 || interpolateV1;
 
-const PaperOnboardingComponent = ({
-  data,
-  safeInsets: _safeInsets,
-  direction = DEFAULT_DIRECTION,
-  // indicator config
-  indicatorSize = DEFAULT_INDICATOR_SIZE,
-  indicatorBackgroundColor = DEFAULT_INDICATOR_BACKGROUND_COLOR,
-  indicatorBorderColor = DEFAULT_INDICATOR_BORDER_COLOR,
-  // override styles
-  titleStyle,
-  descriptionStyle,
-  // close button config
-  closeButton,
-  closeButtonTextStyle,
-  closeButtonText = DEFAULT_CLOSE_BUTTON_TEXT,
-  onCloseButtonPress = DEFAULT_CLOSE_BUTTON_CALLBACK,
-  onIndexChange,
-}: PaperOnboardingProps) => {
-  // state
-  const [dimensions, setDimensions] = useState<PaperOnboardingScreenDimensions>(
+type PaperOnboarding = PaperOnboardingMethods;
+
+const PaperOnboardingComponent = forwardRef<
+  PaperOnboarding,
+  PaperOnboardingProps
+>(
+  (
     {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    }
-  );
-
-  // refs
-  const pagesRef = useRef<Array<Animated.View | null>>(data.map(() => null));
-
-  //#region variables
-  const safeInsets = useMemo<Required<Insets>>(() => {
-    return {
-      top: _safeInsets?.top ?? DEFAULT_SAFE_INSET,
-      bottom: _safeInsets?.bottom ?? DEFAULT_SAFE_INSET,
-      left: _safeInsets?.left ?? DEFAULT_SAFE_INSET,
-      right: _safeInsets?.right ?? DEFAULT_SAFE_INSET,
-    };
-  }, [_safeInsets]);
-  //#endregion
-
-  // memo
-  const { gestureHandler, state, translation, velocity } =
-    usePanGestureHandler();
-
-  const indicatorsContainerLeftPadding = useMemo(() => {
-    const containerLeftPadding = dimensions.width / 2 - indicatorSize / 2;
-    return I18nManager.isRTL
-      ? -containerLeftPadding + indicatorSize * (data.length - 1)
-      : containerLeftPadding;
-  }, [dimensions.width, indicatorSize, data.length]);
-
-  // animations
-  const animatedStaticIndex = useValue(0);
-  const animatedIndex = useTiming({
-    animatedStaticIndex,
-    value: direction === 'horizontal' ? translation.x : translation.y,
-    velocity: direction === 'horizontal' ? velocity.x : velocity.y,
-    state: state,
-    size: data.length,
-    screenWidth: dimensions.width,
-  });
-
-  const indicatorsContainerPosition = data.map(
-    (_, index) => index * indicatorSize * -1
-  );
-  const animatedIndicatorsContainerPosition = add(
-    interpolate(animatedIndex, {
-      inputRange: data.map((_, index) => index),
-      outputRange: I18nManager.isRTL
-        ? indicatorsContainerPosition.reverse()
-        : indicatorsContainerPosition,
-      extrapolate: Animated.Extrapolate.CLAMP,
-    }),
-    indicatorsContainerLeftPadding
-  );
-
-  // callbacks
-  const handlePageRef = useCallback((ref, index) => {
-    pagesRef.current[index] = ref;
-  }, []);
-
-  const handleOnLayout = useCallback(
-    ({
-      nativeEvent: {
-        layout: { width, height },
-      },
-    }: LayoutChangeEvent) => {
-      setDimensions({
-        width,
-        height,
-      });
+      data,
+      safeInsets: _safeInsets,
+      direction = DEFAULT_DIRECTION,
+      // indicator config
+      indicatorSize = DEFAULT_INDICATOR_SIZE,
+      indicatorBackgroundColor = DEFAULT_INDICATOR_BACKGROUND_COLOR,
+      indicatorBorderColor = DEFAULT_INDICATOR_BORDER_COLOR,
+      // override styles
+      titleStyle,
+      descriptionStyle,
+      // close button config
+      closeButton,
+      closeButtonTextStyle,
+      closeButtonText = DEFAULT_CLOSE_BUTTON_TEXT,
+      onCloseButtonPress = DEFAULT_CLOSE_BUTTON_CALLBACK,
+      onIndexChange,
     },
-    []
-  );
+    ref
+  ) => {
+    // state
+    const [dimensions, setDimensions] =
+      useState<PaperOnboardingScreenDimensions>({
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+      });
 
-  //#region effects
-  useCode(
-    () =>
-      onChange(
-        animatedStaticIndex,
-        call([animatedStaticIndex], args => {
-          /**
-           * @DEV
-           * here we directly manipulate pages native props by setting `pointerEvents`
-           * to `auto` for current page and `none` for others.
-           */
-          pagesRef.current.map((pageRef, _index) => {
-            // @ts-ignore
-            pageRef.setNativeProps({
-              pointerEvents: _index === args[0] ? 'auto' : 'none',
+    // refs
+    const indexRef = useRef<number>(0);
+    const pagesRef = useRef<Array<Animated.View | null>>(data.map(() => null));
+
+    //#region variables
+    const safeInsets = useMemo<Required<Insets>>(() => {
+      return {
+        top: _safeInsets?.top ?? DEFAULT_SAFE_INSET,
+        bottom: _safeInsets?.bottom ?? DEFAULT_SAFE_INSET,
+        left: _safeInsets?.left ?? DEFAULT_SAFE_INSET,
+        right: _safeInsets?.right ?? DEFAULT_SAFE_INSET,
+      };
+    }, [_safeInsets]);
+    const indicatorsContainerLeftPadding = useMemo(() => {
+      const containerLeftPadding = dimensions.width / 2 - indicatorSize / 2;
+      return I18nManager.isRTL
+        ? -containerLeftPadding + indicatorSize * (data.length - 1)
+        : containerLeftPadding;
+    }, [dimensions.width, indicatorSize, data.length]);
+    //#endregion
+
+    //#region animated variables
+    const { gestureHandler, state, translation, velocity } =
+      usePanGestureHandler();
+    const animatedStaticIndex = useValue<number>(0);
+    const animatedOverrideIndex = useValue<number>(0);
+    const animatedIndex = useTiming({
+      animatedStaticIndex,
+      animatedOverrideIndex,
+      value: direction === 'horizontal' ? translation.x : translation.y,
+      velocity: direction === 'horizontal' ? velocity.x : velocity.y,
+      state: state,
+      size: data.length,
+      screenWidth: dimensions.width,
+    });
+    const indicatorsContainerPosition = useMemo(
+      () => data.map((_, index) => index * indicatorSize * -1),
+      [data, indicatorSize]
+    );
+    const animatedIndicatorsContainerPosition = useMemo(
+      () =>
+        add(
+          interpolate(animatedIndex, {
+            inputRange: data.map((_, index) => index),
+            outputRange: I18nManager.isRTL
+              ? indicatorsContainerPosition.reverse()
+              : indicatorsContainerPosition,
+            extrapolate: Animated.Extrapolate.CLAMP,
+          }),
+          indicatorsContainerLeftPadding
+        ),
+      [
+        data,
+        animatedIndex,
+        indicatorsContainerLeftPadding,
+        indicatorsContainerPosition,
+      ]
+    );
+    //#endregion
+
+    //#region callbacks
+    const handlePageRef = useCallback((pageRef, index) => {
+      pagesRef.current[index] = pageRef;
+    }, []);
+
+    const handleOnLayout = useCallback(
+      ({
+        nativeEvent: {
+          layout: { width, height },
+        },
+      }: LayoutChangeEvent) => {
+        setDimensions({
+          width,
+          height,
+        });
+      },
+      []
+    );
+    //#endregion
+
+    //#region public methods
+    const handleNavigateToNextPage = useCallback(() => {
+      const currentIndex = indexRef.current;
+      if (currentIndex === data.length - 1) {
+        return;
+      }
+      animatedOverrideIndex.setValue(currentIndex + 1);
+    }, [data, animatedOverrideIndex]);
+    const handleNavigateToPreviousPage = useCallback(() => {
+      const currentIndex = indexRef.current;
+      if (currentIndex === 0) {
+        return;
+      }
+      animatedOverrideIndex.setValue(currentIndex - 1);
+    }, [animatedOverrideIndex]);
+    useImperativeHandle(
+      ref,
+      () => ({
+        next: handleNavigateToNextPage,
+        previous: handleNavigateToPreviousPage,
+      }),
+      [handleNavigateToNextPage, handleNavigateToPreviousPage]
+    );
+    //#endregion
+
+    //#region effects
+    useCode(
+      () =>
+        onChange(
+          animatedStaticIndex,
+          call([animatedStaticIndex], args => {
+            indexRef.current = args[0];
+            /**
+             * @DEV
+             * here we directly manipulate pages native props by setting `pointerEvents`
+             * to `auto` for current page and `none` for others.
+             */
+            pagesRef.current.map((pageRef, _index) => {
+              // @ts-ignore
+              pageRef.setNativeProps({
+                pointerEvents: _index === args[0] ? 'auto' : 'none',
+              });
             });
-          });
 
-          if (onIndexChange) {
-            onIndexChange(args[0]);
-          }
-        })
-      ),
-    []
-  );
-  //#endregion
+            if (onIndexChange) {
+              onIndexChange(args[0]);
+            }
+          })
+        ),
+      []
+    );
+    //#endregion
 
-  // renders
-  return (
-    <PanGestureHandler {...gestureHandler}>
-      <Animated.View onLayout={handleOnLayout} style={styles.container}>
-        <Background
-          animatedIndex={animatedIndex}
-          data={data}
-          safeInsets={safeInsets}
-          screenDimensions={dimensions}
-          indicatorSize={indicatorSize}
-          animatedIndicatorsContainerPosition={
-            animatedIndicatorsContainerPosition
-          }
-        />
-
-        {data.map((item, index) => (
-          <Page
-            key={`page-${index}`}
-            index={index}
-            item={item}
+    // renders
+    return (
+      <PanGestureHandler {...gestureHandler}>
+        <Animated.View onLayout={handleOnLayout} style={styles.container}>
+          <Background
             animatedIndex={animatedIndex}
-            indicatorSize={indicatorSize}
-            titleStyle={titleStyle}
-            descriptionStyle={descriptionStyle}
+            data={data}
             safeInsets={safeInsets}
             screenDimensions={dimensions}
-            handleRef={handlePageRef}
+            indicatorSize={indicatorSize}
+            animatedIndicatorsContainerPosition={
+              animatedIndicatorsContainerPosition
+            }
           />
-        ))}
 
-        <IndicatorsContainer
-          data={data}
-          animatedIndex={animatedIndex}
-          animatedIndicatorsContainerPosition={
-            animatedIndicatorsContainerPosition
-          }
-          indicatorSize={indicatorSize}
-          indicatorBackgroundColor={indicatorBackgroundColor}
-          indicatorBorderColor={indicatorBorderColor}
-          safeInsets={safeInsets}
-        />
+          {data.map((item, index) => (
+            <Page
+              key={`page-${index}`}
+              index={index}
+              item={item}
+              animatedIndex={animatedIndex}
+              indicatorSize={indicatorSize}
+              titleStyle={titleStyle}
+              descriptionStyle={descriptionStyle}
+              safeInsets={safeInsets}
+              screenDimensions={dimensions}
+              handleRef={handlePageRef}
+            />
+          ))}
 
-        <CloseButton
-          data={data}
-          animatedIndex={animatedIndex}
-          safeInsets={safeInsets}
-          closeButtonText={closeButtonText}
-          closeButtonTextStyle={closeButtonTextStyle}
-          closeButton={closeButton}
-          onCloseButtonPress={onCloseButtonPress}
-        />
-      </Animated.View>
-    </PanGestureHandler>
-  );
-};
+          <IndicatorsContainer
+            data={data}
+            animatedIndex={animatedIndex}
+            animatedIndicatorsContainerPosition={
+              animatedIndicatorsContainerPosition
+            }
+            indicatorSize={indicatorSize}
+            indicatorBackgroundColor={indicatorBackgroundColor}
+            indicatorBorderColor={indicatorBorderColor}
+            safeInsets={safeInsets}
+          />
+
+          <CloseButton
+            data={data}
+            animatedIndex={animatedIndex}
+            safeInsets={safeInsets}
+            closeButtonText={closeButtonText}
+            closeButtonTextStyle={closeButtonTextStyle}
+            closeButton={closeButton}
+            onCloseButtonPress={onCloseButtonPress}
+          />
+        </Animated.View>
+      </PanGestureHandler>
+    );
+  }
+);
 
 const PaperOnboarding = memo(PaperOnboardingComponent);
 
